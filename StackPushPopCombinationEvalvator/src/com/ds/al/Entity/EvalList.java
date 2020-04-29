1828e00617c336ef6934c666e05ff1813b9dea7a
Nandan Nayak <nandannayak15@gmail.com>
2020-04-30 02:37:36 +0530

    initial commit

---
+++ StackPushPopCombinationEvalvator/src/com/ds/al/Entity/EvalList.java
@@ -0,0 +1,8 @@
+package com.ds.al.Entity;
+
+public class EvalList {
+
+	private int data;
+	private boolean visited;
+
+}
---
+++ StackPushPopCombinationEvalvator/src/com/ds/al/starter/EvalStart.java
@@ -0,0 +1,19 @@
+package com.ds.al.starter;
+
+import java.util.Scanner;
+
+public class EvalStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int data, m;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the stack data sequentially");
+		m = sc.nextInt();
+		for (int i = 0; i < m; i++) {
+            System.out.println("Enter the data" );
+		}
+
+	}
+
+}

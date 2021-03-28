b0430163e4189ae35359a3981289dd8dabe41064
Nandan Nayak <nandannayak15@gmail.com>
2021-03-29 00:51:54 +0530

    removing used Data Structures

--- LinkedLst/src/com/ds/al/linkedLst/LinkedLstOps.java
+++
@@ -1,8 +0,0 @@
-package com.ds.al.linkedLst;
-
-public interface LinkedLstOps {
-
-	public LinkedLstEntity AddNode(LinkedLstEntity head,int data);
-	public LinkedLstEntity FindCommonNode(LinkedLstEntity head1,LinkedLstEntity head2);
-	
-}

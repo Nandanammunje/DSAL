5fa6079d5d4df161ce497fa61f69027e3a7d5a67
nandan.nayak <nangithubber@gmail.com>
2021-05-21 21:13:06 +0530

    Zig Zag Tree Traversal Code initial commit

--- TreeOps/src/com/ds/al/TreeOps/TreeOpsImpl.java
+++ TreeOps/src/com/ds/al/TreeOps/TreeOpsImpl.java
@@ -3,6 +3,8 @@ package com.ds.al.TreeOps;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static com.ds.al.constants.TreeConstants.NODESEPERATOR;
+
+import com.ds.al.TreeUtility.QueueEntity;
 import com.ds.al.tree.TreeEntity;
 
 public class TreeOpsImpl implements TreeOps {
@@ -10,9 +12,9 @@ public class TreeOpsImpl implements TreeOps {
 	private int headIndex;
 	private TreeEntity root;
 	private int currNodeIndex;
-	private boolean found=false;
+	private boolean found = false;
 	private String AncestorNodes;
-	
+
 	private void getCreateTree(String inorderSubStr, String preOrderSequence, TreeEntity root, int index,
 			TreeEntity head, String inOrderSequence) {
 		// TODO Auto-generated method stub
@@ -72,20 +74,17 @@ public class TreeOpsImpl implements TreeOps {
 		return this.root;
 	}
 
-	
-	public void getAncestorString(String treeNode,TreeEntity Node,String path) {
+	public void getAncestorString(String treeNode, TreeEntity Node, String path) {
 		// TODO Auto-generated method stub
-		if(root==null||Node==null||found)
-		{
-			return ;
+		if (root == null || Node == null || found) {
+			return;
 		}
-		if(treeNode.equals(Character.toString(Node.getData())))
-		{
-			AncestorNodes=path;
-			found=true;
-			return ;
+		if (treeNode.equals(Character.toString(Node.getData()))) {
+			AncestorNodes = path;
+			found = true;
+			return;
 		}
-		path=path+Node.getData()+NODESEPERATOR;
+		path = path + Node.getData() + NODESEPERATOR;
 		getAncestorString(treeNode, Node.getLeft(), path);
 		getAncestorString(treeNode, Node.getRight(), path);
 	}
@@ -93,44 +92,56 @@ public class TreeOpsImpl implements TreeOps {
 	@Override
 	public String[] getAncestors(String treeNode) {
 		// TODO Auto-generated method stub
-		String path="";
-		found=false;
-		AncestorNodes="";
-		String nodes[]=null;
-		getAncestorString(treeNode,root, path);
-		if(AncestorNodes!=null&&!AncestorNodes.isEmpty())
-		{
-			nodes=AncestorNodes.split(",");
+		String path = "";
+		found = false;
+		AncestorNodes = "";
+		String nodes[] = null;
+		getAncestorString(treeNode, root, path);
+		if (AncestorNodes != null && !AncestorNodes.isEmpty()) {
+			nodes = AncestorNodes.split(",");
 		}
-		
+
 		return nodes;
 	}
 
 	@Override
 	public String getLCA(String treeNode1, String treeNode2) {
 		// TODO Auto-generated method stub
-		String lcaNode=null;
-		String ancestralNodes1[]=getAncestors(treeNode1);	
-		String ancestralNodes2[]=getAncestors(treeNode2);
-		if(ancestralNodes1!=null&&ancestralNodes2!=null&&ancestralNodes1.length>0&&ancestralNodes2.length>0)
-		{
-			int commonPathLen=(ancestralNodes1.length > ancestralNodes2.length)?ancestralNodes2.length:ancestralNodes1.length;
-			for(int i=0;i<commonPathLen;i++)
-			{
-				if(ancestralNodes1[i].equals(ancestralNodes2[i]))
-				{
-					lcaNode=ancestralNodes1[i];
+		String lcaNode = null;
+		String ancestralNodes1[] = getAncestors(treeNode1);
+		String ancestralNodes2[] = getAncestors(treeNode2);
+		if (ancestralNodes1 != null && ancestralNodes2 != null && ancestralNodes1.length > 0
+				&& ancestralNodes2.length > 0) {
+			int commonPathLen = (ancestralNodes1.length > ancestralNodes2.length) ? ancestralNodes2.length
+					: ancestralNodes1.length;
+			for (int i = 0; i < commonPathLen; i++) {
+				if (ancestralNodes1[i].equals(ancestralNodes2[i])) {
+					lcaNode = ancestralNodes1[i];
 				}
 			}
 		}
-	   
-		
-		
+
 		return lcaNode;
 	}
-	
-	
 
-	
+	public void zigZagTraversal() throws Exception {
+		QueueEntity queue = new QueueEntity();
+		queue.Enqueue(root);
+		queue.Enqueue(null);
+		TreeEntity treeNode;
+		int i = 1;
+		while (!queue.isEmpty()) {
+			treeNode = queue.Dequeue();
+			if (treeNode == null) {
+				i++;
+				queue.Enqueue(null);
+			}
+			if (treeNode.getLeft() != null)
+				queue.Enqueue(treeNode.getLeft());
+			if (treeNode.getRight() != null)
+				queue.Enqueue(treeNode.getRight());
+
+		}
+	}
 
 }

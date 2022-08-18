6e91b4db9e7a8d57043edae8b4c4788a68e015b0
NanGitHubber <nandan.nayak@nuance.com>
2022-08-19 01:46:44 +0530

    Large number Key finder for Binary Heap

--- BinaryHeap/src/com/ds/al/BinaryHeap/Operations/BinaryHeapOps.java
+++ BinaryHeap/src/com/ds/al/BinaryHeap/Operations/BinaryHeapOps.java
@@ -4,6 +4,7 @@ import com.ds.al.BinaryHeap.Entity.BinaryHeapEntity;
 
 public interface BinaryHeapOps {
 
-	
-	public void printNumbersLessRange(BinaryHeapEntity heapEntity,int range);
+	public void printNumbersLessRange(BinaryHeapEntity heapEntity, int key);
+
+	public void printNumbersMoreRange(BinaryHeapEntity heapEntity, int key);
 }

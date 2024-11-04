e02b2c9f46bbef9f30f4bf7ddf2f083193f5b458
Nandan Nayak <nandannayak@microsoft.com>
2024-11-05 01:42:20 +0530

    optimized rectangle histogram

--- StackPushPopCombinationEvalvator/src/com/ds/al/ops/StackQueueImpl.java
+++ StackPushPopCombinationEvalvator/src/com/ds/al/ops/StackQueueImpl.java
@@ -158,35 +158,76 @@ public class StackQueueImpl {
 	}
 
 	public int FindGreatestRectangleHistogram(int histogram[]) {
-		int totalArr = 0,leftWidth=0,rightWidth=0,area=0;
+		int totalArr = 0, leftWidth = 0, rightWidth = 0, area = 0;
 		int rightArr[] = nextSmallerElementRightIndex(histogram);
 		int leftArr[] = previousSmallElementleftIndex(histogram);
 		leftArr[0] = -2;
-		rightArr[histogram.length-1]=-2;
-		
+		rightArr[histogram.length - 1] = -2;
+
 		for (int i = 0; i < histogram.length; i++) {
-			if(leftArr[i]==-1)
-				leftWidth=i;
-			if(rightArr[i]==-1)
-				rightWidth=histogram.length-i-1;
-			
-			if(leftArr[i]==-2)
-			  leftWidth=0;
-			else if(leftArr[i]!=-1&&leftArr[i]!=-2)
-				leftWidth=i-leftArr[i]-1;
-			if(rightArr[i]==-2)
-				rightWidth=0;
-			else if(rightArr[i]!=-1&&rightArr[i]!=-2)
-				rightWidth=rightArr[i]-i-1;
-		
-				
-			
-			area=((leftWidth+rightWidth)*histogram[i])+histogram[i];
-			totalArr=Math.max(area, totalArr);
+			if (leftArr[i] == -1)
+				leftWidth = i;
+			if (rightArr[i] == -1)
+				rightWidth = histogram.length - i - 1;
+
+			if (leftArr[i] == -2)
+				leftWidth = 0;
+			else if (leftArr[i] != -1 && leftArr[i] != -2)
+				leftWidth = i - leftArr[i] - 1;
+			if (rightArr[i] == -2)
+				rightWidth = 0;
+			else if (rightArr[i] != -1 && rightArr[i] != -2)
+				rightWidth = rightArr[i] - i - 1;
+
+			area = ((leftWidth + rightWidth) * histogram[i]) + histogram[i];
+			totalArr = Math.max(area, totalArr);
 		}
 		return totalArr;
 
 	}
-	
+
+	public int FindGreatestRectangleHistogramOptimized(int histogram[]) {
+		int totalArr = 0, leftWidth = 0, rightWidth = 0, area = 0;
+		Deque<Integer> previousSmallerStack = new LinkedList<Integer>();
+		for (int i = 0; i < histogram.length; i++) {
+			if (previousSmallerStack.isEmpty()) {
+				previousSmallerStack.push(i);
+			} else {
+				while (!previousSmallerStack.isEmpty() && histogram[previousSmallerStack.peek()] >= histogram[i]) {
+                           int nextVal = previousSmallerStack.pop();
+                           int prevVal=(previousSmallerStack.isEmpty())?-1:previousSmallerStack.peek();
+                           if(prevVal==-1)
+                        	   leftWidth=nextVal;
+                           else
+                        	   leftWidth=nextVal-prevVal-1;
+                           rightWidth=i-nextVal-1;
+                         
+                           area = ((leftWidth + rightWidth) * histogram[nextVal]) + histogram[nextVal];
+                           totalArr = Math.max(area, totalArr);	                   
+                          
+					  
+					
+				}
+				 previousSmallerStack.push(i);
+
+			}
+
+		}
+		while(!previousSmallerStack.isEmpty())
+		{
+			
+			int currentIndex=previousSmallerStack.pop();
+			int nextIndex=(previousSmallerStack.isEmpty())?-1:previousSmallerStack.peek();
+			rightWidth=histogram.length-currentIndex-1;
+			if(nextIndex==-1)
+				 leftWidth=currentIndex ;
+			else
+			 leftWidth=currentIndex-nextIndex-1;
+			area=((leftWidth + rightWidth) * histogram[currentIndex]) + histogram[currentIndex];
+			 totalArr = Math.max(area, totalArr);	  
+		}
+		
+        return totalArr;
+	}
 
 }
